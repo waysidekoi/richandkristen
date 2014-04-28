eebfa1d3496a3f768f851b9135e6af10b7df2280
Wayne Yang <waysidekoi@gmail.com>
2014-04-27 23:38:46 -0400

    Moving config to element specific

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,9 +1,2 @@
-$(document).foundation({
-  orbit: {
-    animation: 'slide', // Sets the type of animation used for transitioning between slides, can also be 'fade'
-    timer_speed: 3000,
-    animation_speed: 500, // Sets the amount of time in milliseconds the transition between slides will last
-    slide_number: false,
-    bullets: false
-  }
-});
+$(document).foundation(
+);
