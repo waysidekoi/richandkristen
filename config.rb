50c27569cb1ab4e93d16fe94c11530959575b4f3
Wayne Yang <waysidekoi@gmail.com>
2014-04-29 22:15:29 -0400

    adding image assets to ignore when hashing assets

--- config.rb
+++ config.rb
@@ -100,7 +100,7 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  activate :asset_hash, ignore: [/.*(jpg|png)/]
 
   # Use relative URLs
   # activate :relative_assets
