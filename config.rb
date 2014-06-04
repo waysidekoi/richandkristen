105cfbb06356d6d8614642b75a142ce08b7a25a0
Wayne Yang <waysidekoi@gmail.com>
2014-06-04 03:44:58 -0400

    not caching png's

--- config.rb
+++ config.rb
@@ -107,7 +107,7 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  activate :asset_hash, ignore: [/.*png/]
 
   # Use relative URLs
   # activate :relative_assets
