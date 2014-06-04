87cd617b8ed34a0b3f0211a96a15c16ed1bf7e08
Wayne Yang <waysidekoi@gmail.com>
2014-06-04 00:42:42 -0400

    trying to cache image assets

--- config.rb
+++ config.rb
@@ -107,7 +107,7 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash, ignore: [/.*(jpg|png)/]
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
